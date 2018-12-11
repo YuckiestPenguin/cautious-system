04b13aee8f5d20402a5a7a90fb607b7c81cd15bf
Vico Melo <vinicomelo@gmail.com>
2018-12-11 17:22:24 -0500

    adding sections

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {TextInput} from 'react-native';
 import {Card, CardSection, Button} from "./common";
 
 class LoginForm extends Component {
